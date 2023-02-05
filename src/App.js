6b6d1ef0b3731ad482c8a0f23c60fcb49886c28e
ThanhSon02 <thanhson.hw@gmail.com>
2023-02-05 23:03:11 +0700

    adjust progress bar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import './styles/styles.scss';
 import './App.scss';
-import { useState } from 'react';
-import Song from './components/Song/Song';
 import Player from './components/Player/Player';
 import Playlist from './components/Playlist/Playlist';
 import audios from './assets/audio';
 
 function App() {
-
-    let time = new Date().toLocaleTimeString();
-    const [currentTime, setCurrentTime] = useState(time);
-
-    // useEffect(() => {
-    //     setInterval(() => {
-    //         let time = new Date().toLocaleTimeString();
-    //         setCurrentTime(time)
-    //     }, 1000)
-    // }, []);
-
-
     return (
         <div className="app container-fluid d-flex justify-content-center px-5">
             <div className='row overflow-hidden' style={{width: 1440}}>
 
                 {/* Main section */}
-                <div className='play-section d-flex flex-column align-items-center g-0'>
-                    <div className='play-section__content'>
-                        <h3 className='time text-uppercase text-white'>{currentTime}</h3>
-                        <h4 className='song_name text-uppercase text-white'>Once upon time in new york</h4>
-                        <div className='next-playlist text-white'>
-                            Up next
-                            <Song/>
-                            <Song/>
-                            <Song/>
-                        </div>
-                    </div>
-                    <div className='play-control d-flex justify-content-center align-items-center'>
-                        <Player
-                            tracksSrc={audios}
-                        />
-                    </div>
-                </div>
+                <Player
+                    tracksSrc={audios}
+                />
                 {/* Playlist section */}
                 <div className='playlist-section'>
                     <span>Upcoming content</span>
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,6 +1,5 @@
 import './Player.scss'
 import Control from './Control';
-import ProgressBar from './Progressbar';
 import { useState, useRef, useEffect } from 'react';
 
 function Player({ tracksSrc }) {
@@ -10,12 +9,23 @@ function Player({ tracksSrc }) {
     const [trackProgress, setTrackProgress] = useState(0)
     const audioRef = useRef(new Audio(tracksSrc[0].src))
 
+    const onScrub = (value) => {
+        audioRef.current.currentTime = value
+        setTrackProgress(audioRef.current.currentTime)
+    }
+
+    const trackStyle = `-webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(10, #fff), color-stop(20, #777))`
+
     useEffect(() => {
-        if(isPlaying) {
-            audioRef.current.play()
-        }else {
-            audioRef.current.pause()
+        if (audioRef.current.src) {
+            if(isPlaying) {
+                audioRef.current.play()
+                console.log(audioRef.current.currentTime);
+            }else {
+                audioRef.current.pause()
+            }
         }
+
     },[isPlaying])
 
     const toNextTrack = () => {
@@ -32,16 +42,43 @@ function Player({ tracksSrc }) {
 
 
     return (
-        <div className="container d-flex flex-column justify-content-between">
-            <ProgressBar/>
-            <Control
-                isPlaying={isPlaying}
-                setIsPlaying={setIsPlaying}
-                onNextClick={toNextTrack}
-                onVolumnClick={toAdjustVolumn}
-                onSpeedClick={toAdjustSpeed}
-            />
+
+        <div className='play-section d-flex flex-column align-items-center g-0'>
+            <div className='play-section__content'>
+                <h4 className='song_name text-uppercase text-white'>Once upon time in new york</h4>
+                <div className='next-playlist text-white'>
+                    Up next
+                </div>
+            </div>
+                <div className='play-control d-flex justify-content-center align-items-center'>
+                    <div className="container d-flex flex-column justify-content-between">
+                    <label>
+                        <input
+                            className='progress-bar'
+                            type="range"
+                            step="1"
+                            min="0"
+                            max="100"
+                            onChange={(e) => {onScrub(e.target.value)}}
+                            value={trackProgress}
+                            onInput={(e) => {
+                                console.log(e.target.value)
+                                e.target.style.backgroundSize = e.target.value + '% 100%'
+                            }}
+                        />
+                    </label>
+
+                        <Control
+                            isPlaying={isPlaying}
+                            setIsPlaying={setIsPlaying}
+                            onNextClick={toNextTrack}
+                            onVolumnClick={toAdjustVolumn}
+                            onSpeedClick={toAdjustSpeed}
+                        />
+                </div>
+            </div>
         </div>
+
     );
 }
 
